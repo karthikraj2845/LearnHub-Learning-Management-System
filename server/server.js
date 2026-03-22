fac6979c49e99b5aad2af161ea8cef040cc1e830
PUDURI KARTHIK RAJ <karthikrajpuduri@gmail.com>
2026-03-22 15:40:47 +0530

    importing webhooks

--- server/server.js
+++ server/server.js
@@ -1,22 +1,8 @@
-// import express from 'express';
-// import cors from 'cors';
-// import 'dotenv/config';
-// import connectDB from './configs/mongodb.js';
-// import { clerkWebhooks } from './controllers/webhooks.js';
-// const app = express();
-// await connectDB()
-// app.use(cors());
-// app.get('/', (req, res) => {
-//     res.send('API is working');
-// });
-// app.post('/clerk',express.json(),clerkWebhooks)
-// // const PORT = process.env.PORT || 5000;
-// // app.listen(PORT, () => {
-// //     console.log(`Server is running on port ${PORT}`);
-// // }
-// // );
-// export default app;
 import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './configs/mongodb.js';
+import { clerkWebhooks } from './controllers/webhooks.js';
 
 const app = express();
 
